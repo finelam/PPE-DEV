280cfea9fdc5a99febcc21d9522ccc694f13b817
finelam <finelam@free.fr>
2015-03-18 22:42:48 +0100

    Ajout de la page de login

--- Jismen/routes/index.js
+++ Jismen/routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+})
+.get('/login', function(req, res, next) {
+        res.render('login', { title: 'Login' });
+    });
 
 module.exports = router;
