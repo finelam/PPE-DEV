b17a06e0a47abe890cf98f94d7383531da359c3b
finelam <finelam@free.fr>
2015-05-14 16:28:09 +0200

    Connexion à la bdd 'Jismen', mise en place du cadre des vues, inclusion des éléments js des vues

--- Jismen/app.js
+++ Jismen/app.js
@@ -4,6 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connexion à la DB
+mongoose.connect('mongodb://localhost/jismen', function(err){
+  if (err){
+    console.log('Erreur de connexion' + err);
+  } else {
+    console.log('Connexion à jismen ok !');
+  }
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
