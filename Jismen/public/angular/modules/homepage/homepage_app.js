26c8ef2621a05b6e9934724cd2488bf4dc9ffdc5
finelam <finelam@free.fr>
2015-05-31 17:02:16 +0200

    Carousel sur la homepage

--- Jismen/public/angular/modules/homepage/homepage_app.js
+++ Jismen/public/angular/modules/homepage/homepage_app.js
@@ -6,7 +6,8 @@ homepage.config(['$routeProvider', function($routeProvider){
 			templateUrl: 	'angular/modules/homepage/homepage.html',
 			controller: 	'homepageCtrl' 
 		})
-		.when('/:cat/', {
+
+		.when('/categorie/:cat/', {
 			templateUrl: 	'angular/modules/homepage/subCat.html',
 			controller: 	'subCatCtrl'
 		});
@@ -14,8 +15,22 @@ homepage.config(['$routeProvider', function($routeProvider){
 
 homepage.controller('homepageCtrl', ['$scope', function($scope){
 	$scope.test = 'homepageCtrl dans homepage_app.js';
+	$scope.slides = [
+		{
+			image: 'http://img.cache-cache.fr/products_images/prod_27336/h_robe-sans-manches-dentelle-faux-bustier-cache-cache-NOIR-front-44.jpg',
+			text : 'robe courte'
+		},
+		{
+			image: 'http://img.cache-cache.fr/products_images/prod_20945/g_robe-bustier-decollete-coeur-cache-cache-VERMILLON-onroll-1174.jpg',
+			text : 'robe bustier'
+		},
+		{
+			image: 'http://img.cache-cache.fr/products_images/prod_20985/h_robe-debardeur-droite-cache-cache-noir-onroll-44.jpg',
+			text : 'robe débarddeur'
+		}
+	];
 }]);
 
-homepage.controller('subCatCtrl', ['$scope', function($scope){
-	$scope.test = 'subCatCtrl';
+homepage.controller('subCatCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
+	$scope.test = $routeParams.cat;
 }]);
